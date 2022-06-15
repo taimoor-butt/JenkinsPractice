5e6adba98361053cce4d886481c646244e3b2e89
PostEx-Taimoor Butt <taimoor.ahmad@postex.pk>
2022-06-15 21:24:27 +0500

    first commit

--- src/main/java/com/test/jenkins/Main.java
+++ src/main/java/com/test/jenkins/Main.java
@@ -3,7 +3,6 @@ package com.test.jenkins;
 public class Main {
     public static void main(String[] args) {
         System.out.println("This world is amazing");
-
         System.out.println("This world is amazing");
     }
 }
